0a15a5942c69f07c30ffaa157569f2557136a209
Tomoyuki KANO <tomo@appletz.jp>
2015-12-08 14:46:33 +0900

    initial implements

---
+++ app/controllers/charts_controller.rb
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+class ChartsController < ApplicationController
+
+  SPAN = Math::PI * 4
+  STEP = 0.1
+  STEP_UNIT = 10
+
+  def index
+    
+    
+    @data1 = []  # 配列の場合
+    @data2 = {}  # Hashの場合(User.order('group_id ASC).group(:group_id).count) 等
+    @data3 = {}
+    start = Date.new(2015, 1, 1)
+    0.step(SPAN, STEP) {|r|
+      t = (start + r * STEP_UNIT).to_s
+      @data1 << [t, Math.sin(r)]
+      @data2[t] = Math.sin(r*2)
+      @data3[t] = Math.cos(r)
+    }
+    pp @data1
+  end
+end
---
+++ config/initializers/chartkick.rb
@@ -0,0 +1,6 @@
+Chartkick.options = {
+  height: "400px",
+  colors: ["pink", "#999"],
+  html: '<div id="%{id}" style="height: %{height};">Loading...</div>',
+  # content_for: :chart_js,
+}
